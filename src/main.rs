06dc2268f8d67872e4695663c7b9e145a9dc3a10
Haf <haf@protonmail.ch>
2023-12-06 20:11:34 +0100

    just messin around

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use indoc::indoc;
+
+struct Version(u8, u8, u8);
+
+impl Version {
+    fn major(&self) -> u8 { self.0 }
+    fn minor(&self) -> u8 { self.1 }
+    fn patch(&self) -> u8 { self.2 }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sample = Version(0, 1, 2);
+
+    println!(indoc! {"
+        major: {}
+        minor: {}
+        patch: {}
+    "}, sample.major(), sample.minor(), sample.patch());
 }
